555b3bcab15f8b28da7ef59120f4f30160fd73c6
asantosmanu <manuelasantos452006@gmail.com>
2024-04-11 15:26:43 -0300

    Categorias e Cores feitas

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import router from './router';
     <router-link :to="{ name: 'home' }">Home</router-link> |
     <router-link to="/categorias">Categorias</router-link> |
     <router-link to="/acessorios">Acessórios</router-link> |
+    <router-link to="/cores">Cores</router-link> |
   </header>
   <main>
     <RouterView />
